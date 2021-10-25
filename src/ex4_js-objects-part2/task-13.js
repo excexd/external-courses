e109ca3cd6ea950ff1566c83d6434f2b23ccc21c
excexd <regucloudmusic@gmail.com>
2021-10-25 15:48:31 +0300

    correction

--- src/ex4_js-objects-part2/task-13.js
+++
@@ -1,9 +0,0 @@
-function randomInteger() {
-  const min = 0;
-  const max = 100;
-  const rand = min + Math.random() * (max + 1 - min);
-
-  return Math.floor(rand);
-}
-
-module.exports = randomInteger;
